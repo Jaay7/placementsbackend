2372975e42f510265c1d9e4999923c3b066c60fb
Jaay7 <jayakrishna.madem@gmail.com>
2022-04-02 21:07:06 +0530

    added CORS

--- placementsbackend/settings.py
+++ placementsbackend/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'jobs',
     'studentinfo',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ROOT_URLCONF = 'placementsbackend.urls'
 
