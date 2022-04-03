69fcfce34b91db18efe0beaa0af0dbf7f7822cd1
Jaay7 <jayakrishna.madem@gmail.com>
2022-04-03 19:33:33 +0530

    student details mutation queries updated

--- studentinfo/schema.py
+++ studentinfo/schema.py
@@ -1,4 +1,4 @@
-from graphene import Mutation, ObjectType, String, Field, ID, Int, List, DateTime, Boolean
+from graphene import Float, Mutation, ObjectType, String, Field, ID, Int, List, DateTime, Boolean
 from graphene_django.types import DjangoObjectType
 from .models import StudentDetails
 import graphql_jwt
@@ -9,19 +9,24 @@ class StudentDetailsType(DjangoObjectType):
 
 
 class Query(object):
-  student_details = Field(StudentDetailsType, student_id=String(required=True))
+  student_details = Field(StudentDetailsType, student_id=Int(required=True))
 
+  #resolve student details by student id
   @staticmethod
-  def resolve_student_details(self, info, id):
-    return StudentDetails.objects.get(student_id=id)
+  def resolve_student_details(self, info, **kwargs):
+    student_id = kwargs.get('student_id')
+    return StudentDetails.objects.get(student_id=student_id)
 
 
 class AddStudentDetails(Mutation):
-  student_details = Field(StudentDetailsType)
+  student_id = Int()
+  full_name = String()
+  education = String()
+  current_cgpa = Float()
+  experience = String()
+  skills = String()
 
   class Arguments:
-    student_id = String(required=True)
-    full_name = String(required=True)
     education = String(required=True)
     current_cgpa = String(required=True)
     experience = String(required=True)
@@ -29,8 +34,28 @@ class AddStudentDetails(Mutation):
   
   @staticmethod
   def mutate(self, info, **kwargs):
-    student_details = StudentDetails.objects.create(**kwargs)
-    return AddStudentDetails(student_details=student_details)
+    # verify auth token
+    user = info.context.user
+    if user.is_anonymous:
+      raise Exception('Not logged in!')
+    else:
+      # get the user id
+      user_id = user.id
+      user_full_name = user.full_name
+      student_details = StudentDetails.objects.create(
+        student_id=user_id,
+        student_full_name=user_full_name,
+        **kwargs
+      )
+      return AddStudentDetails(
+        student_id=student_details.student_id,
+        full_name=student_details.student_full_name,
+        education=student_details.education,
+        current_cgpa=student_details.current_cgpa,
+        experience=student_details.experience,
+        skills=student_details.skills,
+      )
+    
 
 class Mutation(ObjectType):
   add_student_details = AddStudentDetails.Field()
