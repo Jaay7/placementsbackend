d77b674324e190dc6c851e16115a2773dec82c46
Jaay7 <jayakrishna.madem@gmail.com>
2022-04-10 16:18:27 +0530

    allow all hosts

--- placementsbackend/settings.py
+++ placementsbackend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-!3j0v_17mx&74vr2pu^fpa^9+=&=-k$d^q##(mz29%x5w#bef!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
