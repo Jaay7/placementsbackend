b03829e0cddfe3174448c0330fa523151a9184b6
Jaay7 <jayakrishna.madem@gmail.com>
2022-05-11 10:30:07 +0530

    updated registered jobs app

--- registeredjobs/manager.py
+++ registeredjobs/manager.py
@@ -7,9 +7,17 @@ class RegisteredJobsManager(BaseUserManager):
     # get user and job details from models and save them
     registration = self.model(**kwargs)
     registration.save()
-    return registration
+    return "Application sent"
   
   def get_job(self, id=None):
     if not id:
       raise ValueError(_('Jobs must have an id'))
     return self.get(pk=id)
+
+  def remove_application(self, id=None):
+    if not id:
+      raise ValueError(_('Jobs must have an id'))
+    else:
+      registration = self.get(pk=id)
+      registration.delete()
+      return "Application removed"
--- registeredjobs/schema.py
+++ registeredjobs/schema.py
@@ -8,8 +8,20 @@ class RegisteredJobsType(DjangoObjectType):
   class Meta:
     model = RegisteredJobs
 
+class Query(object):
+  registered_jobs = List(RegisteredJobsType)
+  #resolve registered jobs by job id
+  @staticmethod
+  def resolve_registered_jobs(self, info, **kwargs):
+    user = info.context.user
+    if user.is_anonymous:
+      raise Exception('Not logged in!')
+    else:
+      # job_id = kwargs.get('job_id')
+      return RegisteredJobs.objects.filter(user=user.id)
+
 class ApplyJob(Mutation):
-  job = Field(RegisteredJobsType)
+  response = String()
 
   class Arguments:
     job_id = ID(required=True)
@@ -21,14 +33,35 @@ class ApplyJob(Mutation):
     if get_user.is_anonymous:
       raise Exception('Not logged in!')
     else:
-      registered_job = RegisteredJobs.objects.apply_job(
+      RegisteredJobs.objects.apply_job(
         user = get_user,
         job = get_job,
         **kwargs
       )
       return ApplyJob(
-        job = registered_job
+        response = "Application sent"
       )
+
+class RemoveApplication(Mutation):
+  response = String()
+
+  class Arguments:
+    apl_id = ID(required=True)
+
+  @staticmethod
+  def mutate(root, info, **kwargs):
+    get_user = info.context.user
+    if get_user.is_anonymous:
+      raise Exception('Not logged in!')
+    else:
+      RegisteredJobs.objects.remove_application(
+        id = kwargs.get('apl_id')
+      )
+      return RemoveApplication(
+        response = "Application removed"
+      )
+
 
 class Mutation(ObjectType):
   apply_job = ApplyJob.Field()
+  remove_application = RemoveApplication.Field()
